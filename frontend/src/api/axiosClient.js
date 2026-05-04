0291f4683d18e29386704efe4cd0c4757671e891
Jefferson Tovar <zjeffer1@gmail.com>
2026-05-04 08:45:36 -0500

    fix: Docker configuration

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
+console.log("ENV:", import.meta.env.VITE_API_URL);
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:5000/api"
 });
-
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 }, (error) => {
   return Promise.reject(error);
